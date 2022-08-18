ac94a51646f4024371f60ef8a64c7ed67b1ee00a
dseveria <dseveria@redhat.com>
2022-08-18 13:09:42 +0200

    fix values canary rollout

---
+++ canary-argo-rollouts/util/delete.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+cd /tmp/deployment/cloud-native-deployment-strategies
+
+oc delete project gitops
+
+oc delete -f blue-green-argo-rollouts/application-shop-blue-green-rollouts.yaml
+
+oc delete -f blue-green-argo-rollouts/application-cluster-config.yaml
+argocd login --core
+oc project openshift-gitops
+argocd app delete argo-rollouts -y
+
+
+oc delete -f gitops/gitops-operator.yaml
+oc delete subscription tekton -n openshift-operators
+oc delete clusterserviceversion openshift-pipelines-operator-rh.v1.6.3 -n openshift-operators
+
+oc delete subscription openshift-gitops-operator -n openshift-operators
+oc delete clusterserviceversion openshift-gitops-operator.v1.5.5 -n openshift-operators
+
+git checkout main
+git branch -d rollouts
+git push origin --delete rollouts
+
+#manual
+#argo app argo-rollouts
+#gitops operator
---
+++ canary-argo-rollouts/util/test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+rm -rf /tmp/deployment
+mkdir /tmp/deployment
+cd /tmp/deployment
+
+git clone https://github.com/davidseve/cloud-native-deployment-strategies.git
+cd cloud-native-deployment-strategies
+git checkout -b canary 
+git push origin canary 
+
+oc apply -f gitops/gitops-operator.yaml
+
+sleep 30s
+
+sed -i '/pipeline.enabled/{n;s/.*/        value: "true"/}' canary-argo-rollouts/application-cluster-config.yaml
+oc apply -f canary-argo-rollouts/application-cluster-config.yaml --wait=true
+
+sleep 1m
+
+sed -i 's/change_me/davidseve/g' canary-argo-rollouts/application-shop-canary-rollouts.yaml
+
+oc apply -f canary-argo-rollouts/application-shop-canary-rollouts.yaml --wait=true
+tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=gitops --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n gitops --showlog
+
+sed -i '/products-blue/{n;n;n;s/.*/    tag: v1.1.1/}' helm/quarkus-helm-umbrella/chart/values/values-rollouts.yaml
+
+git add helm/quarkus-helm-umbrella/chart/values/values-rollouts.yaml
+git commit -m "Change products version to v1.1.1"
+git push origin canary 
