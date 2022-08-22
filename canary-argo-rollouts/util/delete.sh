5f61f4e492d5fe8909b2f89234561aa023b068a3
dseveria <dseveria@redhat.com>
2022-08-22 13:41:07 +0200

    add analysis to canary rollout

--- canary-argo-rollouts/util/delete.sh
+++ canary-argo-rollouts/util/delete.sh
@@ -2,22 +2,10 @@
 
 cd /tmp/deployment/cloud-native-deployment-strategies
 
-oc delete project gitops
 
 oc delete -f canary-argo-rollouts/application-shop-canary-rollouts.yaml
 
-oc delete -f canary-argo-rollouts/application-cluster-config.yaml
-argocd login --core
-oc project openshift-gitops
-argocd app delete argo-rollouts -y
 
-
-oc delete -f gitops/gitops-operator.yaml
-oc delete subscription tekton -n openshift-operators
-oc delete clusterserviceversion openshift-pipelines-operator-rh.v1.6.3 -n openshift-operators
-
-oc delete subscription openshift-gitops-operator -n openshift-operators
-oc delete clusterserviceversion openshift-gitops-operator.v1.5.5 -n openshift-operators
 
 git checkout main
 git branch -d canary
