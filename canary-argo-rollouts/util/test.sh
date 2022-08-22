6bbd38fa82c5eaae44de0efffedab42fbc9911c4
dseveria <dseveria@redhat.com>
2022-08-22 16:23:37 +0200

    revert add analysis to canary rollout

--- canary-argo-rollouts/util/test.sh
+++ canary-argo-rollouts/util/test.sh
@@ -50,7 +50,11 @@ tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --p
 #Rollback
 #this is not neccesary becase argo rollouts do the rollback because of scaleDownDelaySeconds (default 30 seconds), just to make it work I add the sleep
 git revert HEAD --no-edit
+sed -i '/products-blue/{n;n;n;n;n;n;n;n;n;n;N;N;N;N;N;N;d;}' values-canary-rollouts.yaml 
+git add .
+git commit -m "delete steps for rollout"
 git push origin canary
+
 
 status=none
 while [[ "$status" != "Paused - CanaryPauseStep" ]]
