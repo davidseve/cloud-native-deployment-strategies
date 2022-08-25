3950bc8eaecba07aa7ee5c4a74e38de0e2cddb3a
dseveria <dseveria@redhat.com>
2022-08-25 14:11:23 +0200

    add values for pipelues values

--- blue-green-pipeline-environments/util/test.sh
+++ blue-green-pipeline-environments/util/test.sh
@@ -9,7 +9,7 @@ cd /tmp/deployment
 
 git clone https://github.com/davidseve/cloud-native-deployment-strategies.git
 cd cloud-native-deployment-strategies
-git checkout -b blue-green
+git checkout blue-green
 git push origin blue-green
 
 oc apply -f gitops/gitops-operator.yaml
