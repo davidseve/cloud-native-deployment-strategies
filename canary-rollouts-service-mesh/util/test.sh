bb5b67b2485f8734a5d483118e3a6cb6dbfec6c7
dseveria <dseveria@redhat.com>
2022-10-19 13:42:10 +0200

    add argo rollouts wiht service mesh

---
+++ canary-rollouts-service-mesh/util/test.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+rm -rf /tmp/deployment
+mkdir /tmp/deployment
+cd /tmp/deployment
+
+git clone https://github.com/davidseve/cloud-native-deployment-strategies.git
+cd cloud-native-deployment-strategies
+#To work with a branch that is not main. ./test.sh no helm_base
+if [ ${2:-no} != "no" ]
+then
+    git checkout $2
+fi
+git checkout -b canary-mesh 
+git push origin canary-mesh 
+
+oc apply -f gitops/gitops-operator.yaml
+
+#First time we install operators take logger
+if [ ${1:-no} = "no" ]
+then
+    sleep 30s
+else
+    sleep 1m
+fi
+
+sed -i '/pipeline.enabled/{n;s/.*/        value: "true"/}' canary-rollouts-service-mesh/application-cluster-config.yaml
+#To work with a branch that is not main. ./test.sh ghp_JGFDSFIGJSODIJGF no helm_base
+if [ ${2:-no} != "no" ]
+then
+    sed -i "s/HEAD/$2/g" canary-rollouts-service-mesh/application-cluster-config.yaml
+fi
+oc apply -f canary-rollouts-service-mesh/application-cluster-config.yaml --wait=true
+
+#First time we install operators take logger
+if [ ${1:-no} = "no" ]
+then
+    sleep 1m
+else
+    sleep 2m
+fi
+
+sed -i 's/change_me/davidseve/g' canary-rollouts-service-mesh/application-shop-canary-rollouts-mesh.yaml
+
+oc apply -f canary-rollouts-service-mesh/application-shop-canary-rollouts-mesh.yaml --wait=true
+tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=gitops  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n gitops --showlog
+
+sed -i '/products-blue/{n;n;n;n;s/.*/      tag: v1.1.1/}' helm/quarkus-helm-umbrella/chart/values/values-canary-rollouts.yaml
+
+git add helm/quarkus-helm-umbrella/chart/values/values-canary-rollouts.yaml
+git commit -m "Change products version to v1.1.1"
+git push origin canary 
+
+status=none
+while [[ "$status" != "Paused - CanaryPauseStep" ]]
+do
+    sleep 5
+    status=$(kubectlArgo argo rollouts status products -n gitops  --watch=false)
+    echo $status
+done
+
+tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=gitops  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n gitops --showlog
+
+status=none
+while [[ "$status" != "Healthy" ]]
+do
+    sleep 5
+    status=$(kubectlArgo argo rollouts status products -n gitops  --watch=false)
+    echo $status
+done
+tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=gitops  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n gitops --showlog
+
+#Rollback
+#this is not neccesary becase argo rollouts do the rollback because of scaleDownDelaySeconds (default 30 seconds), just to make it work I add the sleep
+git revert HEAD --no-edit
+sed -i '/products-blue/{n;n;n;n;n;n;n;n;n;n;n;n;n;N;N;N;N;N;d;}' helm/quarkus-helm-umbrella/chart/values/values-canary-rollouts.yaml
+sed -i '/products-blue/{n;n;n;n;n;n;n;n;n;n;n;n;s/.*/          - setWeight: 100/}' helm/quarkus-helm-umbrella/chart/values/values-canary-rollouts.yaml
+git add .
+git commit -m "delete steps for rollout"
+git push origin canary-mesh
+
+status=none
+while [[ "$status" != "Healthy" ]]
+do
+    sleep 5
+    status=$(kubectlArgo argo rollouts status products -n gitops  --watch=false)
+    echo $status
+done
+tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=gitops  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n gitops --showlog
