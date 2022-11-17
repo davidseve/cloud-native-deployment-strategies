190fcf9182d67f9bc852ef917ee2a5f7219db9a6
dseveria <dseveria@redhat.com>
2022-11-17 11:14:32 +0100

    set gitops channel gitops-1.6

--- blue-green-pipeline-environments/util/test.sh
+++ blue-green-pipeline-environments/util/test.sh
@@ -17,6 +17,7 @@ fi
 git checkout -b release
 git push origin release
 
+oc login  -u opentlc-mgr -p r3dh4t1! $4
 oc apply -f gitops/gitops-operator.yaml
 
 #First time we install operators take logger
@@ -27,18 +28,14 @@ else
     sleep 1m
 fi
 
-sed -i "s/changeme_token/$1/g" blue-green-pipeline-environments/applicationset-cluster-config.yaml
-sed -i 's/changeme_user/davidseve/g' blue-green-pipeline-environments/applicationset-cluster-config.yaml
-sed -i 's/changeme_mail/davidseve@gmail.com/g' blue-green-pipeline-environments/applicationset-cluster-config.yaml
-sed -i 's/changeme_repository/davidseve/g' blue-green-pipeline-environments/applicationset-cluster-config.yaml
 #To work with a branch that is not main. ./test.sh ghp_JGFDSFIGJSODIJGF no helm_base
 if [ ${3:-no} != "no" ]
 then
-    sed -i "s/HEAD/$3/g" blue-green-pipeline-environments/applicationset-cluster-config.yaml
+    sed -i "s/HEAD/$3/g" blue-green-pipeline-environments/application-cluster-config.yaml
 fi
 
 
-oc apply -f blue-green-pipeline-environments/applicationset-cluster-config.yaml --wait=true
+oc apply -f blue-green-pipeline-environments/application-cluster-config.yaml --wait=true
 
 #First time we install operators take logger
 if [ ${2:-no} = "no" ]
@@ -50,25 +47,28 @@ fi
 
 sed -i 's/change_me/davidseve/g' blue-green-pipeline-environments/applicationset-shop-blue-green.yaml
 
+oc login -u user1 -p openshift $4
+
 oc apply -f blue-green-pipeline-environments/applicationset-shop-blue-green.yaml --wait=true
 
-cd blue-green-pipeline-environments/pipelines/run-products
-namespace=gitops-pre
+export TOKEN=$1
+export GIT_USER=davidseve
+oc create secret generic github-token --from-literal=username=${GIT_USER} --from-literal=password=${TOKEN} --type "kubernetes.io/basic-auth" -n user1-continuous-delivery
+oc annotate secret github-token "tekton.dev/git-0=https://github.com/davidseve" -n user1-continuous-delivery
+oc secrets link pipeline github-token -n user1-continuous-delivery
+
+
+cd blue-green-pipeline-environments/pipelines/run-products-stage
+namespace=user1-stage
 while [[ "$namespace" != "exit" ]]
 do
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
 
     
-    oc create -f 1-pipelinerun-products-new-version.yaml -n $namespace
+    oc create -f 1-pipelinerun-products-new-version.yaml -n user1-continuous-delivery
 
     oc get service products-umbrella-offline -n $namespace --output="jsonpath={.spec.selector.version}" > color
     replicas=-1
-    while [ $replicas != 0 ]
-    do
-        sleep 5
-        replicas=$(oc get deployments products-$(cat color) -n $namespace --output="jsonpath={.spec.replicas}" 2>&1)
-        echo $replicas
-    done
     while [ $replicas != 2 ]
     do
         sleep 5
@@ -77,35 +77,36 @@ do
 
     done
 
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=online --param MODE=offline --param LABEL=.mode --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.products[0].discountInfo.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=offline --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=online --param MODE=offline --param LABEL=.mode --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.products[0].discountInfo.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=offline --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery--showlog
 
     oc create -f 2-pipelinerun-products-switch.yaml -n $namespace
     sleep 20s
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
 
     #Rollback
     oc create -f 2-pipelinerun-products-switch-rollback.yaml -n $namespace
     sleep 20s
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.0.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
 
 
     oc create -f 2-pipelinerun-products-switch.yaml -n $namespace
     sleep 20s
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
 
 
     oc create -f 3-pipelinerun-products-scale-down.yaml -n $namespace
     sleep 40s
 
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=online --param MODE=online --param LABEL=.mode --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.products[0].discountInfo.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=offline --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
-    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n $namespace --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=online --param MODE=online --param LABEL=.mode --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.products[0].discountInfo.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=offline --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
+    tkn pipeline start pipeline-blue-green-e2e-test --param NEW_IMAGE_TAG=v1.1.1 --param MODE=online --param LABEL=.version --param APP=products --param NAMESPACE=$namespace  --param MESH=False --param JQ_PATH=.metadata --workspace name=app-source,claimName=workspace-pvc-shop-cd-e2e-tests -n user1-continuous-delivery --showlog
 
 
-    if [ $namespace = "gitops-pre" ]
+    if [ $namespace = "user1-stage" ]
     then
-        namespace=gitops-prod
+        cd blue-green-pipeline-environments/pipelines/run-products-prod
+        namespace=user1-prod
     else
         namespace=exit
     fi
