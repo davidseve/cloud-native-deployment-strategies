6dc4c891610a555d4c9f20cb1c13c5593738fdea
dseveria <dseveria@redhat.com>
2024-01-04 17:09:58 +0100

    upgrade to gitops-1.11

--- tests/testAll.sh
+++ tests/testAll.sh
@@ -9,22 +9,7 @@ echo $4 #OCP domain
 echo ¡¡blue-green-pipeline!!
 cd ../blue-green-pipeline/util
 
-./test.sh $1 $2 no $3
-sleep 1m
-echo ¡¡Fin blue-green-pipeline!!
-cd ../../blue-green-pipeline/util
-./delete.sh
 
-sleep 30s
-echo ¡¡blue-green-argo-rollouts!!
-cd ../../blue-green-argo-rollouts/util
-./test.sh no $2 no
-sleep 1m
-echo ¡¡Fin blue-green-argo-rollouts!!
-cd ../../blue-green-argo-rollouts/util
-./delete.sh
-
-sleep 30s
 echo ¡¡canary-argo-rollouts!!
 cd ../../canary-argo-rollouts/util
 ./test.sh no $2 no
